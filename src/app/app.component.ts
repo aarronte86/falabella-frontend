b4883c253ef35e71db4767036b5062f4a942c390
Alfredo Arronte <a.arronte.ameneiro@accenture.com>
2020-05-24 21:47:14 -0400

    feat: redirect to landing when browser get refreshed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router, NavigationStart } from "@angular/router";
+
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'falabella-frontend';
+  private _subscription: Subscription;
+
+  constructor(private router: Router) {
+    // Redirect to landing when browser get refreshed and it is not in landing page
+    this._subscription = router.events.subscribe((event) => {
+      if (
+        event instanceof NavigationStart &&
+        !router.navigated &&
+        event.url !== "/"
+      ) {
+        router.navigate(["/"]);
+      }
+    });
+  }
 }
